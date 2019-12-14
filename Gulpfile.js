f705b23425ea4b7c5be69bdaea995d747de29c0e
Sensanaty <sensanati@gmail.com>
2019-12-14 14:57:29 +0800

    Make Gulp minify/uglify javascript with the help of webpack

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const concatCSS = require('gulp-concat-css');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+let babel = require('gulp-babel');
+const webpack = require('webpack-stream');
 
 gulp.task('minify-index', function () {
    return gulp.src('unminified/index/**/*.css')
@@ -19,6 +23,21 @@ gulp.task('minify-about', function () {
        .pipe(gulp.dest('public/styles/'));
 });
 
-gulp.task('default', function () {
-   return gulp.watch('unminified/**/*.css', gulp.series('minify-index', 'minify-about'))
+gulp.task('minify-index-js', function () {
+   return gulp.src('unminified/index/scripts/**/*.js')
+      .pipe(concat('index.js'))
+      .pipe(webpack())
+      .pipe(babel({
+         presets: ["@babel/preset-env"]
+       }))
+      // .pipe(uglify())
+      .pipe(rename('index.js'))
+      .pipe(gulp.dest('public/scripts'));
 });
+
+function watch() {
+   gulp.watch('unminified/**/*.css', gulp.series('minify-index', 'minify-about'));
+   gulp.watch('unminified/**/*.js', gulp.series('minify-index-js'));
+}
+
+gulp.task('default', watch);
