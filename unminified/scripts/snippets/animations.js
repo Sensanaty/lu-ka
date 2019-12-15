acc532f90627c4668d8707d35e45d66af72b9854
Sensanaty <sensanati@gmail.com>
2019-12-15 17:49:29 +0800

    Improve on animation.js, simplify/DRY it

--- unminified/scripts/snippets/animations.js
+++ unminified/scripts/snippets/animations.js
@@ -10,69 +10,62 @@ const ANIMATION_HEIGHT = [{
   value: "20%"
 }, {
   value: "25%"
-},{
+}, {
   value: "30%"
-},{
+}, {
   value: "35%"
-},{
+}, {
   value: "40%"
-},{
+}, {
   value: "45%"
-},{
+}, {
   value: "50%"
-},{
+}, {
   value: "55%"
-},{
+}, {
   value: "60%"
-},{
+}, {
   value: "65%"
-},{
+}, {
   value: "70%"
-},{
+}, {
   value: "75%"
-},{
+}, {
   value: "80%"
-},{
+}, {
   value: "85%"
-},{
+}, {
   value: "90%"
-},{
+}, {
   value: "95%"
-},{
+}, {
   value: "100%"
 }];
 
 const animationBoxes = document.querySelectorAll('.animationBox'); // Select all the boxes to be animated
 
-export function outroAnimation() { // Plays on page out
-  let j = 0;
+let j = 0;
+animationBoxes.forEach(function (box) {
+  box.setAttribute("style", `left: ${j}%;`); // Set their absolute positioning correctly
+  j += 10;
+});
 
-  animationBoxes.forEach(function (box) {
-    box.setAttribute("style", `left: ${j}%;`); // Set their absolute positioning correctly
-    j += 10;
-  });
-
+export function outroAnimation() {
   return anime({
     targets: animationBoxes,
     height: ANIMATION_HEIGHT,
-    delay: anime.stagger(100, {
+    delay: anime.stagger(50, {
       start: 200,
       from: "center"
     }),
     duration: 175,
     easing: 'easeOutElastic',
     elasticity: 500,
+    autoplay: false
   });
 }
 
 export function introAnimation() { // Plays on page load
-  let j = 0;
-
-  animationBoxes.forEach(function (box) {
-    box.setAttribute("style", `left: ${j}%;`); // Set their absolute positioning correctly
-    j += 10;
-  });
-
   return anime({
     targets: animationBoxes,
     height: ANIMATION_HEIGHT,
@@ -85,9 +78,4 @@ export function introAnimation() { // Plays on page load
     elasticity: 500,
     direction: 'reverse'
   });
-}
-
-function inAndOut() {
-  setTimeout(outroAnimation, 1);
-  setTimeout(introAnimation, 1500);
 }
