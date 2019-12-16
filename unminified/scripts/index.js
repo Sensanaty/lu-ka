7c9c76675b33e6977e52b1fffb2fd01f28f0f76b
Sensanaty <sensanati@gmail.com>
2019-12-17 01:10:44 +0800

    idk man

--- unminified/scripts/index.js
+++ unminified/scripts/index.js
@@ -1,35 +1,45 @@
-import { outroAnimation } from './snippets/animations.js';
+import {
+  outroAnimation,
+  introAnimation
+} from './snippets/animations.js';
 
-if (window.location.pathname + window.location.search == "/public/") { // When in root
-  const about = document.querySelector(".about-button");
-  const projects = document.querySelector(".projects-button");
-  const contact = document.querySelector(".contact-button");
-  const ramblings = document.querySelector(".todo-button");
+// Remember to delete /public and .html for production
+if (window.location.pathname + window.location.search == "/public/" || window.location.pathname + window.location.search == "/public/index.html") {
+  const buttons = document.querySelectorAll(".body-button");
 
-  about.addEventListener('click', function (event) {
-    event.preventDefault();
-    handleLinkClick(this);
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+      event.preventDefault();
+      handleLinkClick(this);
+    });
   });
 
-  projects.addEventListener('click', function (event) {
-    event.preventDefault();
-    handleLinkClick(this);
-  });
+} else if (window.location.pathname + window.location.search == "/public/about.html") {
+  const pageLinks = document.querySelectorAll('.link');
+  const animBox = document.querySelectorAll('.animationBox');
 
-  contact.addEventListener('click', function (event) {
-    event.preventDefault();
-    handleLinkClick(this);
-  });
+  if (String(window.performance.getEntriesByType("navigation")[0].type) === "back_forward" || String(window.performance.getEntriesByType("navigation")[0].type) === "reload") {
+    // Check if navigation was performed via a page refresh or navigation with forward/back
+    animBox.forEach(box => box.style.height = "0%");
+  } else setTimeout(introAnimation, 200);
 
-  ramblings.addEventListener('click', function (event) {
-    event.preventDefault();
-    handleLinkClick(this);
+  // After animation
+  // TODO: For some reason this will make the animation play in reverse?
+  pageLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      handleLinkClick(this);
+    });
   });
 }
 
+/**
+ * When a user clicks any link with any href, play outroAnimation and trigger a page navigation after 750 milliseconds
+ * @param {NodeList} object - A NodeList that contains every link on the current page
+ */
 function handleLinkClick(object) {
   outroAnimation();
   setTimeout(() => {
     window.location.href = object.getAttribute("href");
-  }, 750);
+  }, 800);
 }
