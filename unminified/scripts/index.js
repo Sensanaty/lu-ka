f47c433164bedbd7460ec9ae7893a8bdeccbb795
Sensanaty <sensanati@gmail.com>
2019-12-17 17:22:48 +0800

    Remove animations, because it's too complex to work on right now

--- unminified/scripts/index.js
+++ unminified/scripts/index.js
@@ -1,45 +1,19 @@
-import {
-  outroAnimation,
-  introAnimation
-} from './snippets/animations.js';
-
-// Remember to delete /public and .html for production
-if (window.location.pathname + window.location.search == "/public/" || window.location.pathname + window.location.search == "/public/index.html") {
-  const buttons = document.querySelectorAll(".body-button");
-
-  buttons.forEach(function (button) {
-    button.addEventListener('click', function (event) {
-      event.preventDefault();
-      handleLinkClick(this);
-    });
-  });
-
-} else if (window.location.pathname + window.location.search == "/public/about.html") {
-  const pageLinks = document.querySelectorAll('.link');
-  const animBox = document.querySelectorAll('.animationBox');
-
-  if (String(window.performance.getEntriesByType("navigation")[0].type) === "back_forward" || String(window.performance.getEntriesByType("navigation")[0].type) === "reload") {
-    // Check if navigation was performed via a page refresh or navigation with forward/back
-    animBox.forEach(box => box.style.height = "0%");
-  } else setTimeout(introAnimation, 200);
-
-  // After animation
-  // TODO: For some reason this will make the animation play in reverse?
-  pageLinks.forEach(function (link) {
-    link.addEventListener('click', function (event) {
-      event.preventDefault();
-      handleLinkClick(this);
-    });
-  });
-}
-
-/**
- * When a user clicks any link with any href, play outroAnimation and trigger a page navigation after 750 milliseconds
- * @param {NodeList} object - A NodeList that contains every link on the current page
- */
-function handleLinkClick(object) {
-  outroAnimation();
-  setTimeout(() => {
-    window.location.href = object.getAttribute("href");
-  }, 800);
-}
+console.log(`%c
+ _        _    _   _  __                 __  __   ______ 
+| |      | |  | | | |/ /     /\\         |  \\/  | |  ____|
+| |      | |  | | | ' /     /  \\        | \\  / | | |__   
+| |      | |  | | |  <     / /\\ \\       | |\\/| | |  __|  
+| |____  | |__| | | . \\   / ____ \\   _  | |  | | | |____ 
+|______|  \\____/  |_|\\_\\ /_/    \\_\\ (_) |_|  |_| |______|
+\n\t\t\t   %cMade with %c❤️ %cby %cLuka Salević %caka %cSensanaty\n\t\t\t\t\t\t\t\t\t\t\t %c(on Github)\n`,
+       "font-size: 15px; color: #e69900;",
+       "color: #26FD3C; font-size: 13px;",
+       "color: red",
+       "color: #26FD3C; font-size: 13px;",
+       "color: #26FD3C; font-size: 15px;",
+       "color: #26FD3C; font-size: 13px;",
+       "color: #26FD3C; font-size: 15px;",
+       "color: #26FD3C; font-size: 9px;");
+   console.log(
+       `\t\t\t\t\t\t%cCurious about the source? It's on GitHub!\n\t\t\t\t\t   https://github.com/Sensanaty/lu-ka`,
+       "color: #FFA500; font-size: 13px;");
